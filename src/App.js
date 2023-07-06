e7b1e8d8cb359582fdc42c992b1351893d65cb42
irzal <121763889+mathjsx@users.noreply.github.com>
2023-07-07 00:35:32 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,59 +1,37 @@
 import './App.css';
-import { MathJaxContext, MathJax } from 'better-react-mathjax';
+import MySoal1 from './MySoal1.jsx';
+// import { MathJaxContext,MathJax } from "better-react-mathjax";
+
 function App() {
-const config = {
-  loader: { load: ["input/asciimath"] },
-  asciimath: {
-    displaystyle: true,
-    delimiters: [
-      ["$", "$"],
-      ["`", "`"]
-    ]
-  }
-};
+//  const config = {
+//    loader: { load: ["input/asciimath"] },
+//    asciimath: {
+//      displaystyle: true,
+//      delimiters: [
+//        ["$", "$"],
+//        ["`", "`"]
+//      ]
+//    }
+//  };
+
+
   return (
+    <>
     
-    <div className="App">
-  
-      <header className="App-header">
-        <h1>BPHY</h1>
-        <div>
-        <MathJaxContext version={3} config={config}>
-        <MathJax hideUntilTypeset={"first"}>
-
-<ol type="1">
-<li> Bilangan berpangkat</li>
-<li> Pertidaksamaan  Linier</li>
-<li> Barisan dan Deret</li>
-<li> Statistika</li>
-<li> Persamaan kuadrat</li>
-<li> Persamaan lingkaran</li>
-<li> Matrik</li>
-<li> Logika matematika</li>
-<li> Program linier</li>
-<li> Geometri</li>
-<li> Transformasi</li>
-<li> Ruang 3 D</li>
-<li> Peluang</li>
-<li> Limit</li>
-<li> Deferensial</li>
-<li> Integral</li>
-
-</ol>
-
-
-        
-
-        
-                                                              
-        </MathJax>
-        </MathJaxContext>
-      </div>
-    
-     
-      </header>
-      
-    </div>
+    <header className="App-header">
+        <h1 className='text-3xl font-bold underline'>BPHY</h1>
+        <br/>
+        <hr/>
+    </header>
+    <main className="h-auto bg-slate-200 overflow-scroll">
+          <MySoal1 />
+    </main>
+    <footer className="App-header">
+        <h1 className='text-2xl font-bold '>2023</h1>
+        <br/>
+        <hr/>
+    </footer>
+    </>
   );
 }
 
